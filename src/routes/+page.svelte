490ef473d88a8a68ce79732694a546f9ef1f5ef6
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-10-10 23:13:31 +0100

    FEAT (shadcn): adds shadcn config and Card component
    Implements the Card component on main page with a bit of Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<main class="md:container md:mx-auto flex-col justify-center content-center py-32">
+
+    <Card.Root>
+        <Card.Header>
+            <Card.Title>Welcome to TEMPLATE_PROJECT</Card.Title>
+            <Card.Description>Card Description</Card.Description>
+        </Card.Header>
+        <Card.Content>
+            <p>Card Content</p>
+        </Card.Content>
+        <Card.Footer>
+            <p>Card Footer</p>
+        </Card.Footer>
+    </Card.Root>
+</main>
